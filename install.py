b76eae6ebff21e8063b566a208cc70231e3c1436
mblackhamgw <morrisb@xmission.com>
2018-03-20 16:35:18 -0600

    Add files via upload

--- install.py
+++ install.py
@@ -3,7 +3,7 @@
 import os, sys, logging, rpm, socket
 from subprocess import Popen, PIPE
 import install_lib  as gmslib
-
+from time import sleep
 
 
 def loggingSetup():
@@ -19,60 +19,61 @@ def loggingSetup():
                         filename=logfile,
                         filemode='w')
 
+
 def main():
+    gms.logit('Welcome to the Micro Focus GroupWise Mobility Service install.')
+    gms.whoami()
+    newinstall = gms.gmsExits()
+    # print 'new = %s' % newinstall
+    gms.logit('slesUpgrade = %s' % gms.ifSlesUpgrade())
     scriptInstallFile = "/var/log/datasync/script.inst"
     rpmswitch = ''
     if os.path.isfile(scriptInstallFile):
         rpmswitch = '--replacefiles'
     slesUpgrage = False
-    #gms.showlicense()
-    gms.whoami()
-    #
-    #newinstall= gms.gmsExits()
-    #print 'new = %s' % newinstall
-    #print 'slesUpgrade = %s' % gms.ifSlesUpgrade()
-
-    #print gmsrpms
-
-    #for key, value in rpms.iteritems():
-    #    print key
-    #    print value
-
-    #gms.installpostgres()
-
-    rpms = gms.getrpminfo()
-    for key, value in rpms.iteritems():
-        if key == 'ImageMagick':
-            imInstall = True
-            instIm = gms.installImageMagick()
-        else:
-            imInstall = False
-
-
-
-
-#    print imInstall
-    #gms.installImageMagick()
-    checkIm = gms.checkImageMagick(imInstall)
-
-    #import rpm signing key
-
     importkey = gms.importrpmkey()
+    gms.showlicense()
 
-    ##check if the network setting are correctly configured
+    gms.logit('Starting GMS installation.')
+
+    #check if the network setting are correctly configured
     fqdn = gms.fqdn()
-    print fqdn
-    #gw18 = gms.promptForProperGWVersion()
-    gw18 = 'yes'
+    gms.logit('Server Hostname : %s' % fqdn)
+
+    gw18 = gms.promptForProperGWVersion()
+    #gw18 = 'yes'
     if gw18 == 'no':
         sys.exit()
 
-    sqlinstall = gms.installPsql()
-    for line in sqlinstall:
-        print line
+    #get rpm info
+    rpms = gms.getrpminfo()
 
-    gmsrpms = gms.rpmlist()
+    #install postgres
+    pginstall = gms.installpostgres()
 
+
+
+    # install ImageMagick
+    for key, value in rpms.iteritems():
+        if key == 'ImageMagick':
+            imInstall = True
+        else:
+            imInstall = False
+
+#    print imInstall
+    gms.installImageMagick()
+    #checkIm = gms.checkImageMagick(imInstall)
+    sleep(2)
+
+#install unixODBC
+    odbcinstall = gms.installUnixODBC()
+    #print odbcinstall
+    sleep(2)
+# install pythoh-ldap
+    ldapinstall = gms.installLdap()
+    sleep(2)
+
+ #   gmsrpms = gms.rpmlist()
     rpminstall = gms.installRpmsFromdisk()
 
 
@@ -83,3 +84,4 @@ if __name__ == '__main__':
     loggingSetup()
     gms = gmslib.GMSINSTALL()
     main()
+
